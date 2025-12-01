f66fcb0c5ad86afbff2bfac2d70024524445abaa
csh <18476979428@163.com>
2025-12-01 22:30:18 +0800

    [13]:competition

---
+++ 13.competition/app/competition.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+//./a.out /dev/device_number 1 这样使用
+int main(int argc,char *argv[])
+{
+        int fd;
+        char buf[32] = {0};
+        fd = open(argv[1], O_RDWR);
+        if (fd < 0) {
+                printf("open is error\n");
+                return -1;
+        }
+        printf("open is ok\n");
+        
+        if(strcmp(argv[2],"topeet")==0)
+                write(fd, argv[2], 10);
+        if(strcmp(argv[2],"itop")==0)
+                write(fd, argv[2], 10);
+        close(fd);
+        return 0;
+}
