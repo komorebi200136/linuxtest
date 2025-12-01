c0e1220b25a9b5cd11513b86cd095273247cde93
csh <18476979428@163.com>
2025-12-01 23:23:47 +0800

    [14]:spinlock

---
+++ 14.spinlock/app/spinlock.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+//./a.out /dev/device_number 1 这样使用
+int main(int argc,char *argv[])
+{
+        int fd;
+        char buf[32] = {0};
+        fd = open(argv[1], O_RDWR);
+        if (fd < 0) {
+                printf("open is error\n");
+                return -1;
+        }
+        printf("open is ok\n");
+        sleep(5);
+        close(fd);
+        return 0;
+}
