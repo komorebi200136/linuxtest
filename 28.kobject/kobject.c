60b8b4b0f796bc059c06b42a3e839bf385f979b0
csh <18476979428@163.com>
2025-12-15 22:36:43 +0800

    [28]:kobject

---
+++ 28.kobject/kobject.c
@@ -0,0 +1,49 @@
+#include "linux/gpio/consumer.h"
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/kobject.h>
+#include <linux/slab.h>
+
+struct kobject *kobject01;
+struct kobject *kobject02;
+struct kobject *kobject03;
+
+// 定义了一个 kobj_type 结构体变量 mytype，用于描述 kobject 的类型。
+struct kobj_type mytype;
+static int __init mykobject_init(void)
+{
+	int ret;
+	//创建kobject的第一种方法
+	kobject01 = kobject_create_and_add("kobject01", NULL);
+	if (!kobject01) {
+		printk(KERN_ERR "Failed to create kobject01\n");
+		return -ENOMEM;
+	}
+	kobject02 = kobject_create_and_add("kobject02", kobject01);
+	if (!kobject02) {
+		printk(KERN_ERR "Failed to create kobject02\n");
+		return -ENOMEM;
+	}
+	//创建kobject的第二种方法
+	kobject03 = kzalloc(sizeof(struct kobject), GFP_KERNEL);
+	ret = kobject_init_and_add(kobject03, &mytype, NULL, "%s","kobject03");
+	if(ret<0) {
+		printk(KERN_ERR "Failed to create kobject03\n");
+		return -ENOMEM;
+	}
+	return 0;
+}
+
+static void __exit mykobject_exit(void)
+{
+	kobject_put(kobject01);
+	kobject_put(kobject02);
+	kobject_put(kobject03);
+        printk("kobject exit\n");
+}
+
+module_init(mykobject_init);
+module_exit(mykobject_exit);
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("csh");
